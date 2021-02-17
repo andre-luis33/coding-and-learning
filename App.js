5fe640d3f79278646b5781c5a747596bdc2a711e
Andre <andreluismoura22@gmail.com>
2021-02-17 20:26:39 -0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <View style={styles.container}>
+         <ScrollView>
+            <Text style={styles.texts}>A lot of cards</Text>   
+            <Text style={styles.texts}>A lot of cards</Text>    
+         </ScrollView>      
+         {/* <StatusBar style="auto" /> */}
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      display: 'flex',
+      height: 300
+   },
+   texts: {
+      textAlign: 'left',
+      backgroundColor: 'red',
+      // alignSelf: 'center'
+      height: 50,
+      padding: 10,
+      display: 'flex',
+      alignItems: 'center',
+   }
 });
